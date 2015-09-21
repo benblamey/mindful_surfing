5169861a0b15811b482f900b6068c177671cd9a1
Ben Blamey <blamey.ben@gmail.com>
2015-09-21 15:55:32 +0200

    check whitelist async correctly.

--- backgroundPage.js
+++ backgroundPage.js
@@ -6,8 +6,10 @@ var white_list = [
 
 var startupTime;
 
+console.log("-- STARTING EXTENSION --");
+
 // This event fires when Chrome starts.
-chrome.runtime.onStartup.addListener(init());
+chrome.runtime.onStartup.addListener(init);
 
 // This event fires when the extension is installed or reloaded.
 chrome.runtime.onInstalled.addListener(init)
@@ -78,68 +80,70 @@ function alarm_fired(alarm) {
 	console.log(alarm);
 
 	// TODO: should return a boolean
-	updateActive();
-	if (!active) {
-		return;
-	}
-	
-	if (alarm.name === 'first alarm') {
-		chrome.notifications.create('foo', {
-			iconUrl: 'icon.png',
-			type: 'basic',
-			title: 'time running out',
-			message: '2 minutes til browser shutdown',
-		});
-	}
-	if (alarm.name === 'second alarm') {
-		chrome.notifications.create('foo', {
-			iconUrl: 'icon.png',
-			type: 'basic',
-			title: 'time running out',
-			message: '1 minute til browser shutdown!',
-		});
-	}
-	if (alarm.name === 'final alarm') {
-		chrome.windows.getAll(function(windows) {
-			console.log(windows);
-			for (i = 0; i < windows.length; i++) {
-				console.log('Closing window ' + windows[i].id + '...');
-				//chrome.windows.remove(windows[i].id);
-			}
+	updateActive(function(active) {
+		if (!active) {
+			return;
+		}
+		
+		if (alarm.name === 'first alarm') {
+			chrome.notifications.create('foo', {
+				iconUrl: 'icon.png',
+				type: 'basic',
+				title: 'time running out',
+				message: '2 minutes til browser shutdown',
+			});
+		}
+		if (alarm.name === 'second alarm') {
+			chrome.notifications.create('foo', {
+				iconUrl: 'icon.png',
+				type: 'basic',
+				title: 'time running out',
+				message: '1 minute til browser shutdown!',
+			});
+		}
+		if (alarm.name === 'final alarm') {
+			chrome.windows.getAll(function(windows) {
+				console.log(windows);
+				for (i = 0; i < windows.length; i++) {
+					console.log('Closing window ' + windows[i].id + '...');
+					//chrome.windows.remove(windows[i].id);
+				}
+				
+			});
+		}
+		if (alarm.name === 'regular') {
+			msSinceStartup = (new Date()) - startupTime;
+			console.log("startup time:");
+			console.log(startupTime);
+			console.log("msSinceStartup:");
+			console.log(msSinceStartup);
 			
-		});
-	}
-	if (alarm.name === 'regular') {
-		msSinceStartup = (new Date()) - startupTime;
-		console.log("startup time:");
-		console.log(startupTime);
-		console.log("msSinceStartup:");
-		console.log(msSinceStartup);
-		
-		
-		var days = Math.floor(msSinceStartup / (1000 * 60 * 60 * 24));
-		msSinceStartup -=  days * (1000 * 60 * 60 * 24);
-		var hours = Math.floor(msSinceStartup / (1000 * 60 * 60));
-		msSinceStartup -= hours * (1000 * 60 * 60);
-		var mins = Math.floor(msSinceStartup / (1000 * 60));
-		msSinceStartup -= mins * (1000 * 60);
-		var seconds = Math.floor(msSinceStartup / (1000));
-		msSinceStartup -= seconds * (1000);
-		durationString = days + " days, " + hours + " hours, " + mins + " minutes, " + seconds + " seconds";
-		
-		chrome.notifications.create('foo', {
-			iconUrl: 'icon.png',
-			type: 'basic',
-			title: 'Mindfulness',
-			message: 'you have been browsing for ' + durationString + "\n\n" + "Time for a break?",
-		});
-	}
+			
+			var days = Math.floor(msSinceStartup / (1000 * 60 * 60 * 24));
+			msSinceStartup -=  days * (1000 * 60 * 60 * 24);
+			var hours = Math.floor(msSinceStartup / (1000 * 60 * 60));
+			msSinceStartup -= hours * (1000 * 60 * 60);
+			var mins = Math.floor(msSinceStartup / (1000 * 60));
+			msSinceStartup -= mins * (1000 * 60);
+			var seconds = Math.floor(msSinceStartup / (1000));
+			msSinceStartup -= seconds * (1000);
+			durationString = days + " days, " + hours + " hours, " + mins + " minutes, " + seconds + " seconds";
+			
+			chrome.notifications.create('foo', {
+				iconUrl: 'icon.png',
+				type: 'basic',
+				title: 'Mindfulness',
+				message: 'you have been browsing for ' + durationString + "\n\n" + "Time for a break?",
+			});
+		}
+	});
 	
-});
+}
 
 // TODO: should return a boolean
-function updateActive() {
+function updateActive(callback) {
 	console.log('updateActie()');
+	var active = true;
 	chrome.windows.getAll({populate: true}, function(windows) {
 		console.log(windows);
 		for (i = 0; i < windows.length; i++) {
@@ -167,5 +171,7 @@ function updateActive() {
 				}
 			}
 		}
+		
+		callback(active);
 	});
 }
