15db28ee685f19cad1fe4a372c0f6a848c5d134c
Ben Blamey <blamey.ben@gmail.com>
2015-09-21 16:01:50 +0200

    Refactored ms duration >string

--- backgroundPage.js
+++ backgroundPage.js
@@ -118,16 +118,7 @@ function alarm_fired(alarm) {
 			console.log("msSinceStartup:");
 			console.log(msSinceStartup);
 			
-			
-			var days = Math.floor(msSinceStartup / (1000 * 60 * 60 * 24));
-			msSinceStartup -=  days * (1000 * 60 * 60 * 24);
-			var hours = Math.floor(msSinceStartup / (1000 * 60 * 60));
-			msSinceStartup -= hours * (1000 * 60 * 60);
-			var mins = Math.floor(msSinceStartup / (1000 * 60));
-			msSinceStartup -= mins * (1000 * 60);
-			var seconds = Math.floor(msSinceStartup / (1000));
-			msSinceStartup -= seconds * (1000);
-			durationString = days + " days, " + hours + " hours, " + mins + " minutes, " + seconds + " seconds";
+			durationString = MSToString(msSinceStartup);
 			
 			chrome.notifications.create('foo', {
 				iconUrl: 'icon.png',
@@ -138,6 +129,34 @@ function alarm_fired(alarm) {
 		}
 	});
 	
+}
+
+function MSToString(msSinceStartup) {
+	var durationString = "";
+	
+	var days = Math.floor(msSinceStartup / (1000 * 60 * 60 * 24));
+	msSinceStartup -=  days * (1000 * 60 * 60 * 24);
+	if (days > 0) {
+			durationString += days + " days, ";
+	}
+	
+	var hours = Math.floor(msSinceStartup / (1000 * 60 * 60));
+	msSinceStartup -= hours * (1000 * 60 * 60);
+	if (durationString != '' || hours > 0) {
+			durationString += hours + " hours, ";
+	}
+	
+	var mins = Math.floor(msSinceStartup / (1000 * 60));
+	msSinceStartup -= mins * (1000 * 60);
+	if (durationString != '' || mins > 0) {
+		durationString += mins + " mins, ";
+	}
+	
+	var seconds = Math.floor(msSinceStartup / (1000));
+	msSinceStartup -= seconds * (1000);
+	durationString += seconds + " seconds";
+	
+	return durationString;
 }
 
 // TODO: should return a boolean
